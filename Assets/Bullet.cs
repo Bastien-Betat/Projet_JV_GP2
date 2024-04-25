399b299ca3c98cd2dc4332920b8460ebf2933e65
Oasis64 <bastien.betat@mmibordeaux.com>
2024-04-25 13:18:00 +0200

    Update Bullet.cs

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collision2D collision)
     {
         
-    }
+        if(collision.gameObject.TryGetComponent<EnemyD>(out EnemyD enemyDComponent))
+        {
+            enemyDComponent.TakeDamage(1);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject);
     }
 }
